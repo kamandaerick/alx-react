a45b01fd8c5b44cc9408d0f6463e9bb408322836
Erick Kamanda <info.proresearchers@gmail.com>
2024-10-29 02:28:00 +0300

    Task 0 revised

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,6 +1,8 @@
-import $ from "jquery";
+import $ from 'jquery';
 
-// Add paragraphs to the body
-$('body').append('<p>Holberton Dashboard</p>');
-$('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<p>Copyright - Holberton School</p>');
+// Create three paragraphs
+$(document).ready(() => {
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<p>Copyright - Holberton School</p>');
+});
