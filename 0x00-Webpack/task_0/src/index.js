3088ffd5dd4c136e846d436b194ce67441c703e7
Erick Kamanda <info.proresearchers@gmail.com>
2024-11-08 15:41:41 +0300

    Task 0: Basic Webpack Set up

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,5 +1,9 @@
-const $ = require( "jquery" );
+import $ from 'jquery';
 
-$('body').append('<p>Holberton Dashboard</p>');
-$('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<p>Copyright - Holberton School</p>');
+const p1 = $('p').text('Holberton Dashboard');
+const p2 = $('p').text('Dashboard data for the students');
+const p3 = $('p').text('Copyright - Holberton School');
+
+p1.appendTo('body');
+p2.appendTo('body');
+p3.appendTo('body');
