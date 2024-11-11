8b306edf33e28c3a8c2ad1d3bef5915970d87948
Erick Kamanda <info.proresearchers@gmail.com>
2024-11-11 20:59:50 +0300

    Install Enzyme

--- 0x01-react_intro/task_3/dashboard/src/setupTests.js
+++ 0x01-react_intro/task_3/dashboard/src/setupTests.js
@@ -2,11 +2,9 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-// import '@testing-library/jest-dom';
+import '@testing-library/jest-dom';
+const Enzyme = require('enzyme');
+const EnzymeAdapter = require('enzyme-adapter-react-15');
 
-// setupTests.js
-
-import { configure } from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-
-configure({ adapter: new Adapter() });
+// Setup enzyme's react adapter
+Enzyme.configure({ adapter: new EnzymeAdapter() });
