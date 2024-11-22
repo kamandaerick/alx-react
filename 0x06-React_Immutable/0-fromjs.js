155b8847e5e05f5d496451322fc99c4e8f4ce9d8
Erick Kamanda <info.proresearchers@gmail.com>
2024-11-22 14:14:20 +0300

    Correction

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,7 +1,5 @@
-const {fromJS} = require("immutable");
+import { fromJS } from "./node_modules/immutable/dist/immutable";
 
-export default function getImmutableObject (object) {
-
+export default function getImmutableObject(object) {
   return fromJS(object);
-
 }
