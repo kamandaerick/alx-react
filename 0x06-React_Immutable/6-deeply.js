ab479a90438c84349cf7b4d2ff04a33ee544da1f
Erick Kamanda <info.proresearchers@gmail.com>
2024-11-22 22:02:42 +0300

    Nested merge

---
+++ 0x06-React_Immutable/6-deeply.js
@@ -0,0 +1,8 @@
+import { Map } from 'immutable';
+
+export default function mergeDeeplyElements(page1, page2) {
+  const map1 = Map(page1);
+  const map2 = Map(page2);
+  const mergedMap = map1.mergeDeep(map2);
+  return mergedMap.toList();
+}
