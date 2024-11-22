ac1e3bedb0225ae5d6800f79ee39e07728629868
Erick Kamanda <info.proresearchers@gmail.com>
2024-11-22 14:11:21 +0300

    Correct eslint error

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,5 +1,7 @@
-const { fromJS } = require('immutable');
+const {fromJS} = require("immutable");
 
-export default function getImmutableObject(object) {
+export default function getImmutableObject (object) {
+
   return fromJS(object);
+
 }
