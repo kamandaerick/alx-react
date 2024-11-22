5f221243418cc5ea4c40b48c320a29e506d020d9
Erick Kamanda <info.proresearchers@gmail.com>
2024-11-22 21:37:53 +0300

    Merge and concat

---
+++ 0x06-React_Immutable/5-merge.js
@@ -0,0 +1,11 @@
+import { Map, List } from 'immutable';
+
+export function concatElements(page1, page2) {
+  return List(page1).concat(List(page2));
+}
+
+export function mergeElements(page1, page2) {
+  const map1 = Map(page1);
+  const map2 = Map(page2);
+  return map1.merge(map2).toList();
+}
